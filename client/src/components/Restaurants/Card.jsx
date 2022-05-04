fcc824ecb8db53ae6f564a9c1423aef856a8ceb7
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-05-03 23:49:03 -0700

    hide env value

--- client/src/components/Restaurants/Card.jsx
+++ client/src/components/Restaurants/Card.jsx
@@ -37,7 +37,7 @@ const Card = ({data, opens, id, lat, long, setIsCurrent}) => {
       categories: categoriesArr
     }
     try {
-      const res = await axios.post('http://localhost:8888/api/favourites/add', newFavourite);
+      const res = await axios.post('/api/favourites/add', newFavourite);
       setFavouritesList([...favouritesList, res.data])
     } catch (error) {
       console.log(error)
@@ -52,7 +52,7 @@ const Card = ({data, opens, id, lat, long, setIsCurrent}) => {
       id: data.id
     }
     try {
-      const res = await axios.post('http://localhost:8888/api/favourites/delete', removeItem);
+      const res = await axios.post('/api/favourites/delete', removeItem);
       const filtered = favouritesList.filter(list => list.id !== res.data.id)
       setFavouritesList(filtered)
     } catch (error) {
@@ -63,7 +63,7 @@ const Card = ({data, opens, id, lat, long, setIsCurrent}) => {
   useEffect(() => {
     const getMyFavourites = async() => {
       try {
-        const allFavourites = await axios.get('http://localhost:8888/api/favourites')
+        const allFavourites = await axios.get('/api/favourites')
         setFavouritesList(allFavourites.data)
       } catch (error) {
         console.log(error)
