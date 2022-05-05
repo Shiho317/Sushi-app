938e7e7a5c9c43bfd36e64c4d32e44b3968bb859
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-05-04 20:00:39 -0700

    added details contents

--- client/src/components/Favourite/Item.jsx
+++ client/src/components/Favourite/Item.jsx
@@ -9,19 +9,21 @@ const Item = ({item}) => {
         <img src={item.image} alt='suki-img'/>
       </div>
       <div className='item-details'>
-        <h2>{item.name}</h2>
-        <div className='rate-price'>
-          <AiFillStar/>
-          <p className='rate'>{item.rate}</p>
-          <p className='price'>{item.price}</p>
-        </div>
-        <div className='category-wrap'>
-          {item.categories.map((category, index) => (
-            <p key={index} className='item-category'>{category}</p>
-          ))}
-        </div>
-        <div className='item-address'>
-          <p>{item.address}</p>
+        <div className='details-contents'>
+          <h2>{item.name}</h2>
+          <div className='rate-price'>
+            <AiFillStar/>
+            <p className='rate'>{item.rate}</p>
+            <p className='price'>{item.price}</p>
+          </div>
+          <div className='category-wrap'>
+            {item.categories.map((category, index) => (
+              <p key={index} className='item-category'>{category}</p>
+            ))}
+          </div>
+          <div className='item-address'>
+            <p>{item.address}</p>
+          </div>
         </div>
         <a href={item.url} target="_blank" rel="noopener noreferrer">
           <button className='details-btn'>
